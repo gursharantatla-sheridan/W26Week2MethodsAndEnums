ae5b6efc33c8f883bf2e9d2efc143adee38c627f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 15:42:17 -0500

    enums

--- IntroToEnums/Program.cs
+++ IntroToEnums/Program.cs
@@ -7,12 +7,36 @@
             Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat
         }
 
+        enum CardValue
+        {
+            Two = 2,
+            Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+            Jack, Queen, King, Ace
+        }
+
+        enum Suit
+        {
+            Heart, Diamond, Club, Spade
+        }
+
+
         static void Main(string[] args)
         {
-            Console.Write("Enter day number (1-7): ");
-            Day day = (Day)int.Parse(Console.ReadLine());
+            Random rnd = new Random();
 
-            Console.WriteLine("\nIt is " + day);
+            for (int i = 0; i < 7; i++)
+            {
+                CardValue cardValue = (CardValue)rnd.Next(2, 15);
+                Suit suit = (Suit)rnd.Next(4);
+
+                Console.WriteLine(cardValue + " of " + suit);
+            }
+
+
+            //Console.Write("Enter day number (1-7): ");
+            //Day day = (Day)int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\nIt is " + day);
         }
     }
 }
