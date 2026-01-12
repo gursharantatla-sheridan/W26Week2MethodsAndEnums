5cc140b25af4ff563589fdc16414325caf683fa9
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 13:36:29 -0500

    value - ref

--- W26Week2MethodsAndEnums/Program.cs
+++ W26Week2MethodsAndEnums/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 5;
+            string str = "hello";
+            int[] arr = { 1, 2, 3 };
+
+            Console.WriteLine("Before changes:");
+            Console.WriteLine("num = " + num);
+            Console.WriteLine("str = " + str);
+            Console.WriteLine("arr[0] = " + arr[0]);
+
+            ChangeValues(num, str, arr);
+
+            Console.WriteLine("\nAfter changes:");
+            Console.WriteLine("num = " + num);
+            Console.WriteLine("str = " + str);
+            Console.WriteLine("arr[0] = " + arr[0]);
+        }
+
+        static void ChangeValues(int n, string s, int[] a)
+        {
+            n = 100;
+            s = "bye";
+            a[0] = 100;
         }
     }
 }
