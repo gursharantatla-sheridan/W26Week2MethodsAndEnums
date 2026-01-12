c92803e74cef4af5249256485262ee2390d89b7f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 14:03:04 -0500

    output param

--- W26Week2MethodsAndEnums/Program.cs
+++ W26Week2MethodsAndEnums/Program.cs
@@ -13,15 +13,34 @@
             Console.WriteLine("str = " + str);
             Console.WriteLine("arr[0] = " + arr[0]);
 
-            ChangeValues(num, str, arr);
+            ChangeValues(ref num, str, arr);
 
             Console.WriteLine("\nAfter changes:");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
             Console.WriteLine("arr[0] = " + arr[0]);
+            Console.WriteLine("\n\n");
+
+            // output paramaters example
+            int rad = 5;
+            double area, circum;
+
+            AreaAndCircum(rad, out area, out circum);
+
+            Console.WriteLine("Area = " + area);
+            Console.WriteLine("Circum = " + circum);
         }
 
-        static void ChangeValues(int n, string s, int[] a)
+        // output parameters
+        static void AreaAndCircum(int r, out double area, out double circum)
+        {
+            area = Math.PI * r * r;
+            circum = 2 * Math.PI * r;
+        }
+
+
+
+        static void ChangeValues(ref int n, string s, int[] a)
         {
             n = 100;
             s = "bye";
