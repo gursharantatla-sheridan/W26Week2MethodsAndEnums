38bbad260e6f470ae38ca8af839fe41171dbf94b
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 13:14:08 -0500

    Add project files.

---
+++ W26Week2MethodsAndEnums/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace W26Week2MethodsAndEnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
