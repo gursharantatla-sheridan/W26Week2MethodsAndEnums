11b66d0200152e1c6ec3d484b7cf080b079435c7
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 15:31:30 -0500

    enum Day

---
+++ IntroToEnums/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IntroToEnums
+{
+    internal class Program
+    {
+        enum Day
+        {
+            Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day number (1-7): ");
+            Day day = (Day)int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nIt is " + day);
+        }
+    }
+}
