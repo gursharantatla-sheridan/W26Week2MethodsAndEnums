256400cf955f68337b28c877ae2048ea88aec9ae
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-12 15:22:44 -0500

    methods

--- W26Week2MethodsAndEnums/Program.cs
+++ W26Week2MethodsAndEnums/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace W26Week2MethodsAndEnums
+﻿using System.Xml.Serialization;
+
+namespace W26Week2MethodsAndEnums
 {
     internal class Program
     {
@@ -23,9 +25,9 @@
 
             // output paramaters example
             int rad = 5;
-            double area, circum;
+            //double area, circum;
 
-            AreaAndCircum(rad, out area, out circum);
+            AreaAndCircum(rad, out double area, out double circum);
 
             Console.WriteLine("Area = " + area);
             Console.WriteLine("Circum = " + circum);
@@ -33,14 +35,56 @@
 
 
 
-            Console.Write("Enter a number: ");
-            int n;
+            //Console.Write("Enter a number: ");
+            ////int n;
 
-            if (int.TryParse(Console.ReadLine(), out n))
-                Console.WriteLine("n = " + n);
-            else
-                Console.WriteLine("Invalid input, please try again");
+            //if (int.TryParse(Console.ReadLine(), out int n))
+            //    Console.WriteLine("n = " + n);
+            //else
+            //    Console.WriteLine("Invalid input, please try again");
+
+            //Console.WriteLine("\n\n");
+
+
+
+            // parameter arrays
+            int a = 4, b = 6;
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.WriteLine("a = {0} b = {1}", a, b);
+
+
+            int[] myArr = { 3, 45, 6, 8, 6, 5, 43, 3 };
+            PrintArray(myArr);
+            PrintArray(3, 4, 5, 6, 76, 8, 9, 87, 76, 65, 5, 4, 3, 4, 5, 66, 7, 6, 8);
+
+
+
+            PersonInfo("John", 23);
+            PersonInfo(name: "John", age: 23);
+            PersonInfo(age: 32, name: "John");
+            PersonInfo("John");
         }
+
+
+        // named arguments / optional args
+        static void PersonInfo(string name, int age = 18)
+        {
+            // method body
+        }
+
+
+
+        // parameter array
+        static void PrintArray(params int[] arr)
+        {
+            Console.WriteLine("\nNumber of items: " + arr.Length);
+
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+
+
 
         // output parameters
         static void AreaAndCircum(int r, out double area, out double circum)
